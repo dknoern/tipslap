0a32058a246cafeef24fdfbfed16b4af39402a5f
David Knoernschild <david@seattleweb.com>
2025-03-24 10:54:44 -0700

    add logging to worker get API

--- app/api/workers/[id]/route.ts
+++ app/api/workers/[id]/route.ts
@@ -10,16 +10,19 @@ export async function GET(
     const client = await clientPromise
     const db = client.db()
     const users = db.collection('users')
+
+    console.log("worker API: looking for worker with ID", params.id)
     
     const worker = await users.findOne({ 
       _id: new ObjectId(params.id),
     })
     
     if (!worker) {
+      console.log("worker API: WORKER NOT FOUND for ID", params.id)
       return NextResponse.json({ error: 'Worker not found' }, { status: 404 })
     }
 
-    console.log("\n\n\n\nWORKER FOUND", worker, "\n\n\n\n")
+    console.log("worker API: WORKER FOUND", worker)
     
     return NextResponse.json(worker)
   } catch (error) {
